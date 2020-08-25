3aa8d3a17ab23884b5935e2d9faeb5412727b968
adityaab14 <adityaab14@gmail.com>
2020-08-25 16:03:02 +0530

    Update fakenews.py

--- fakenews.py
+++ fakenews.py
@@ -24,17 +24,17 @@ print(head1)
 labels=df.label
 print(labels)
 
-#DataFlair - Split the dataset
+#Split the dataset
 x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
-#DataFlair - Initialize a TfidfVectorizer
+#Initialize a TfidfVectorizer
 tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
-#DataFlair - Fit and transform train set, transform test set
+#Fit and transform train set, transform test set
 tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
 tfidf_test=tfidf_vectorizer.transform(x_test)
-#DataFlair - Initialize a PassiveAggressiveClassifier
+#Initialize a PassiveAggressiveClassifier
 pac=PassiveAggressiveClassifier(max_iter=100)
 pac.fit(tfidf_train,y_train)
-#DataFlair - Predict on the test set and calculate accuracy
+#Predict on the test set and calculate accuracy
 y_pred=pac.predict(tfidf_test)
 score=accuracy_score(y_test,y_pred)
 print(f'Accuracy: {round(score*100,2)}%')
